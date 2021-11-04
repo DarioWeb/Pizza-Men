42b22f8f58d18d51b9be6ae2e2199781895d51d3
DarioWeb <93431365+DarioWeb@users.noreply.github.com>
2021-11-04 23:08:49 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$(document).ready(function(){
+    $("#open_menu").click(function(){
+        $("nav").css("width","240px")
+    });
+    $("#close_menu").click(function(){
+        $("nav").css("width","0")
+    });
 
+    let sirina = $(window).width()
+
+    if(sirina <= 991){
+        $("#md_7").removeClass("col-md-7");
+        $("#md_7").addClass("col-md-12");
+    }
+    if(sirina <= 767){
+        $(".order_now").addClass("btn-warning");
+    }
+    
+});
+ 
